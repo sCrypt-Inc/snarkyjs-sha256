33f89d1b868446f017e8303022524138b4a2c8de
hh <hh@scrypt.io>
2022-08-08 15:22:06 +0800

    minimal reproducible example

--- src/sha256.ts
+++ src/sha256.ts
@@ -110,66 +110,76 @@ export default class Sha256 extends Circuit {
     const w25 = Sha256.mod_add(w09, Sha256.s0(w10), w18, Sha256.s1(w23));
     const w26 = Sha256.mod_add(w10, Sha256.s0(w11), w19, Sha256.s1(w24));
     const w27 = Sha256.mod_add(w11, Sha256.s0(w12), w20, Sha256.s1(w25));
-    const w28 = Sha256.mod_add(w12, Sha256.s0(w13), w21, Sha256.s1(w26));
-    const w29 = Sha256.mod_add(w13, Sha256.s0(w14), w22, Sha256.s1(w27));
-    const w30 = Sha256.mod_add(w14, Sha256.s0(w15), w23, Sha256.s1(w28));
-    const w31 = Sha256.mod_add(w15, Sha256.s0(w16), w24, Sha256.s1(w29));
-    const w32 = Sha256.mod_add(w16, Sha256.s0(w17), w25, Sha256.s1(w30));
-    const w33 = Sha256.mod_add(w17, Sha256.s0(w18), w26, Sha256.s1(w31));
-    const w34 = Sha256.mod_add(w18, Sha256.s0(w19), w27, Sha256.s1(w32));
-    const w35 = Sha256.mod_add(w19, Sha256.s0(w20), w28, Sha256.s1(w33));
-    const w36 = Sha256.mod_add(w20, Sha256.s0(w21), w29, Sha256.s1(w34));
-    const w37 = Sha256.mod_add(w21, Sha256.s0(w22), w30, Sha256.s1(w35));
-    const w38 = Sha256.mod_add(w22, Sha256.s0(w23), w31, Sha256.s1(w36));
-    const w39 = Sha256.mod_add(w23, Sha256.s0(w24), w32, Sha256.s1(w37));
-    const w40 = Sha256.mod_add(w24, Sha256.s0(w25), w33, Sha256.s1(w38));
-    const w41 = Sha256.mod_add(w25, Sha256.s0(w26), w34, Sha256.s1(w39));
-    const w42 = Sha256.mod_add(w26, Sha256.s0(w27), w35, Sha256.s1(w40));
-    const w43 = Sha256.mod_add(w27, Sha256.s0(w28), w36, Sha256.s1(w41));
-    const w44 = Sha256.mod_add(w28, Sha256.s0(w29), w37, Sha256.s1(w42));
-    const w45 = Sha256.mod_add(w29, Sha256.s0(w30), w38, Sha256.s1(w43));
-    const w46 = Sha256.mod_add(w30, Sha256.s0(w31), w39, Sha256.s1(w44));
-    const w47 = Sha256.mod_add(w31, Sha256.s0(w32), w40, Sha256.s1(w45));
-    const w48 = Sha256.mod_add(w32, Sha256.s0(w33), w41, Sha256.s1(w46));
-    const w49 = Sha256.mod_add(w33, Sha256.s0(w34), w42, Sha256.s1(w47));
-    const w50 = Sha256.mod_add(w34, Sha256.s0(w35), w43, Sha256.s1(w48));
-    const w51 = Sha256.mod_add(w35, Sha256.s0(w36), w44, Sha256.s1(w49));
-    const w52 = Sha256.mod_add(w36, Sha256.s0(w37), w45, Sha256.s1(w50));
-    const w53 = Sha256.mod_add(w37, Sha256.s0(w38), w46, Sha256.s1(w51));
-    const w54 = Sha256.mod_add(w38, Sha256.s0(w39), w47, Sha256.s1(w52));
-    const w55 = Sha256.mod_add(w39, Sha256.s0(w40), w48, Sha256.s1(w53));
-    const w56 = Sha256.mod_add(w40, Sha256.s0(w41), w49, Sha256.s1(w54));
-    const w57 = Sha256.mod_add(w41, Sha256.s0(w42), w50, Sha256.s1(w55));
-    const w58 = Sha256.mod_add(w42, Sha256.s0(w43), w51, Sha256.s1(w56));
-    const w59 = Sha256.mod_add(w43, Sha256.s0(w44), w52, Sha256.s1(w57));
-    const w60 = Sha256.mod_add(w44, Sha256.s0(w45), w53, Sha256.s1(w58));
-    const w61 = Sha256.mod_add(w45, Sha256.s0(w46), w54, Sha256.s1(w59));
-    const w62 = Sha256.mod_add(w46, Sha256.s0(w47), w55, Sha256.s1(w60));
-    const w63 = Sha256.mod_add(w47, Sha256.s0(w48), w56, Sha256.s1(w61));
+    const s0 = Sha256.s0(w13);
+    //const s1 = Sha256.s1(w26);
+
+    const r2_0 = Sha256.or(Sha256.shiftL(w26, 15), Sha256.shiftR(w26, 17));
+    const r2_1 = Sha256.or(Sha256.shiftL(w26, 13), Sha256.shiftR(w26, 19));
+    const r2_2 = Sha256.shiftR(w26, 10);
+
+    //If you comment the following line of code, `generateKeypair()` will run successfully
+    let aa = r2_0.value.toBits();
+
+
+
+    
+    //const w28 = Sha256.mod_add(w12, Sha256.s0(w13), w21, Sha256.s1(w26));
+    // const w29 = Sha256.mod_add(w13, Sha256.s0(w14), w22, Sha256.s1(w27));
+    // const w30 = Sha256.mod_add(w14, Sha256.s0(w15), w23, Sha256.s1(w28));
+    // const w31 = Sha256.mod_add(w15, Sha256.s0(w16), w24, Sha256.s1(w29));
+    // const w32 = Sha256.mod_add(w16, Sha256.s0(w17), w25, Sha256.s1(w30));
+    // const w33 = Sha256.mod_add(w17, Sha256.s0(w18), w26, Sha256.s1(w31));
+    // const w34 = Sha256.mod_add(w18, Sha256.s0(w19), w27, Sha256.s1(w32));
+    // const w35 = Sha256.mod_add(w19, Sha256.s0(w20), w28, Sha256.s1(w33));
+    // const w36 = Sha256.mod_add(w20, Sha256.s0(w21), w29, Sha256.s1(w34));
+    // const w37 = Sha256.mod_add(w21, Sha256.s0(w22), w30, Sha256.s1(w35));
+    // const w38 = Sha256.mod_add(w22, Sha256.s0(w23), w31, Sha256.s1(w36));
+    // const w39 = Sha256.mod_add(w23, Sha256.s0(w24), w32, Sha256.s1(w37));
+    // const w40 = Sha256.mod_add(w24, Sha256.s0(w25), w33, Sha256.s1(w38));
+    // const w41 = Sha256.mod_add(w25, Sha256.s0(w26), w34, Sha256.s1(w39));
+    // const w42 = Sha256.mod_add(w26, Sha256.s0(w27), w35, Sha256.s1(w40));
+    // const w43 = Sha256.mod_add(w27, Sha256.s0(w28), w36, Sha256.s1(w41));
+    // const w44 = Sha256.mod_add(w28, Sha256.s0(w29), w37, Sha256.s1(w42));
+    // const w45 = Sha256.mod_add(w29, Sha256.s0(w30), w38, Sha256.s1(w43));
+    // const w46 = Sha256.mod_add(w30, Sha256.s0(w31), w39, Sha256.s1(w44));
+    // const w47 = Sha256.mod_add(w31, Sha256.s0(w32), w40, Sha256.s1(w45));
+    // const w48 = Sha256.mod_add(w32, Sha256.s0(w33), w41, Sha256.s1(w46));
+    // const w49 = Sha256.mod_add(w33, Sha256.s0(w34), w42, Sha256.s1(w47));
+    // const w50 = Sha256.mod_add(w34, Sha256.s0(w35), w43, Sha256.s1(w48));
+    // const w51 = Sha256.mod_add(w35, Sha256.s0(w36), w44, Sha256.s1(w49));
+    // const w52 = Sha256.mod_add(w36, Sha256.s0(w37), w45, Sha256.s1(w50));
+    // const w53 = Sha256.mod_add(w37, Sha256.s0(w38), w46, Sha256.s1(w51));
+    // const w54 = Sha256.mod_add(w38, Sha256.s0(w39), w47, Sha256.s1(w52));
+    // const w55 = Sha256.mod_add(w39, Sha256.s0(w40), w48, Sha256.s1(w53));
+    // const w56 = Sha256.mod_add(w40, Sha256.s0(w41), w49, Sha256.s1(w54));
+    // const w57 = Sha256.mod_add(w41, Sha256.s0(w42), w50, Sha256.s1(w55));
+    // const w58 = Sha256.mod_add(w42, Sha256.s0(w43), w51, Sha256.s1(w56));
+    // const w59 = Sha256.mod_add(w43, Sha256.s0(w44), w52, Sha256.s1(w57));
+    // const w60 = Sha256.mod_add(w44, Sha256.s0(w45), w53, Sha256.s1(w58));
+    // const w61 = Sha256.mod_add(w45, Sha256.s0(w46), w54, Sha256.s1(w59));
+    // const w62 = Sha256.mod_add(w46, Sha256.s0(w47), w55, Sha256.s1(w60));
+    // const w63 = Sha256.mod_add(w47, Sha256.s0(w48), w56, Sha256.s1(w61));
 
 
 
 
-    Circuit.asProver(() => {
-      // const w = [
-      //   w00, w01, w02, w03, w04, w05, w06, w07,
-      //   w08, w09, w10, w11, w12, w13, w14, w15,
-      //   w16, w17, w18, w19, w20, w21, w22, w23,
-      //   w24, w25, w26, w27, w28, w29, w30, w31,
-      //   w32, w33, w34, w35, w36, w37, w38, w39,
-      //   w40, w41, w42, w43, w44, w45, w46, w47,
-      //   w48, w49, w50, w51, w52, w53, w54, w55,
-      //   w56, w57, w58, w59, w60, w61, w62, w63
-      // ];
+    // Circuit.asProver(() => {
+    //   const w = [
+    //     w00, w01, w02, w03, w04, w05, w06, w07,
+    //     w08, w09, w10, w11, w12, w13, w14, w15,
+    //     w16, w17, w18, w19, w20, w21, w22, w23,
+    //     w24, w25, w26, w27, // w28, w29, w30, w31,
+    //     // w32, w33, w34, w35, w36, w37, w38, w39,
+    //     // w40, w41, w42, w43, w44, w45, w46, w47,
+    //     // w48, w49, w50, w51, w52, w53, w54, w55,
+    //     // w56, w57, w58, w59, w60, w61, w62, w63
+    //   ];
 
-      // for (let i = 0; i < 21; i++) {
-      //   console.log('wi', i, w[i].toString());
-      // }
+    //   for (let i = 0; i < w.length; i++) {
+    //     console.log('wi', i, w[i].toString());
+    //   }
 
-      //console.log('w06', w06.toString(), 'w07', w07.toString(), 'w15', w15.toString(), 'w20', w20.toString());
-
-
-    })
+    // })
     console.log('g end.')
 
   }
@@ -233,11 +243,6 @@ export default class Sha256 extends Circuit {
     return UInt32.from(Field.ofBits(aa.map((a, i) => a.and(bb[i]))))
   }
 
-  static not(a: UInt32): UInt32 {
-    let aa = a.value.toBits().slice(0, 32);
-    return UInt32.from(Field.ofBits(aa.map(a => a.not())))
-  }
-
   static xor(a: UInt32, b: UInt32, i: number = -1): UInt32 {
 
 
@@ -245,38 +250,10 @@ export default class Sha256 extends Circuit {
 
     let bb = b.value.toBits().slice(0, 32);
 
-
-    const ab = UInt32.from(Field.ofBits(aa.map((a, i) => a.or(bb[i]))))
-
-    const nota = Sha256.not(a);
-
-    const notb = Sha256.not(b);
-
-    const notab = Sha256.or(nota, notb);
-
-
-
-    let ab_u32 = ab.value.toBits().slice(0, 32);
-
-    let notab_u32 = notab.value.toBits().slice(0, 32);
-
-    const r = UInt32.from(Field.ofBits(ab_u32.map((a, i) => a.and(notab_u32[i]))))
-
-    return r;
-
-    
-
-    //return Sha256.and(Sha256.or(a, b), Sha256.or(Sha256.not(a), Sha256.not(b)))
-
-    // (A NAND (A NAND B)) NAND (B NAND (A NAND B))
-    //return Sha256.nand(Sha256.nand(a, Sha256.nand(a,b)), Sha256.nand(b, Sha256.nand(a, b)));
+    return UInt32.from(Field.ofBits(aa.map((itema, i) => {
+      return Circuit.if(itema.equals(bb[i]), Bool(false), Bool(true))
+    })))
   }
-
-
-  // static nand(a: UInt32, b: UInt32): UInt32 {
-  //   // A NAND B = NOT(A AND B)
-  //   return Sha256.not(Sha256.and(a, b))
-  // }
 
 }
 
@@ -288,14 +265,15 @@ async function main() {
   await isReady
   const kp = Sha256.generateKeypair();
 
-  const preimage = Field.fromNumber(1);
-  const preimageSize = UInt32.fromNumber(128);
-  // const hash = Poseidon.hash([preimage]);
-  // console.log(hash.toString())
-  const pi = Sha256.prove([preimage, preimageSize], [Field.fromNumber(4)], kp);
-  console.log('proof', pi);
-  const success = Sha256.verify([Field.fromNumber(4)], kp.verificationKey(), pi)
-  console.log('verify', success);
+  console.log('generateKeypair successfully...')
+  // const preimage = Field.fromNumber(1);
+  // const preimageSize = UInt32.fromNumber(128);
+  // // const hash = Poseidon.hash([preimage]);
+  // // console.log(hash.toString())
+  // const pi = Sha256.prove([preimage, preimageSize], [Field.fromNumber(4)], kp);
+  // console.log('proof', pi);
+  // const success = Sha256.verify([Field.fromNumber(4)], kp.verificationKey(), pi)
+  // console.log('verify', success);
 
   await shutdown();
 }
